01fb7ac6f64ac849c72a2f0188fd3e1a1e79b8de
Dan Cheail <dan@dnch.io>
2019-06-02 16:48:19 +1000

    Stop using old-style stderr pipe redirect

--- fish/functions/git_dirty_status.fish
+++ fish/functions/git_dirty_status.fish
@@ -1,5 +1,5 @@
 function git_dirty_status
-	if [ -z (command git status --porcelain ^/dev/null | tail -n1) ]
+	if [ -z (command git status --porcelain 2> /dev/null | tail -n1) ]
 		echo (set_color 9c3)" ±"(set_color normal)
 	else
 		echo (set_color c03)" ±"(set_color normal)
