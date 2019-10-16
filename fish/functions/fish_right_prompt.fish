e8386a5f57d8d531e2a6afb0724f1bc83a33c5a0
Dan Cheail <dan@dnch.io>
2019-10-17 01:01:49 +1100

    using own powerline in right prompt

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -1,3 +1,7 @@
 function fish_right_prompt
-  starship prompt
+  set items (string trim (starship module git_status))
+  set items $items (string trim (starship module git_state))
+  set items $items (string trim (starship module git_branch))
+
+  powerline --tone=dark --fade=in --direction=right $items
 end
