d6fd73ff5038c8852745040057c1773f17550d4a
Dan Cheail <dan@dnch.io>
2019-10-16 19:05:46 +1100

    Set fish_colours

--- fish/colors.fish
+++ fish/colors.fish
@@ -1,56 +1,59 @@
 # the default color
 # set fish_color_normal
 
-# # the color for commands
-# set fish_color_command
+# don't underline paths
+set fish_color_valid_path normal
 
-# # the color for quoted blocks of text
+# the color for commands
+set fish_color_command 7EAAC7
+
+# the color for quoted blocks of text
 # set fish_color_quote
 
-# # the color for IO redirections
-# set fish_color_redirection
+# the color for IO redirections
+set fish_color_redirection yellow
 
-# # the color for process separators like ';' and '&'
-# set fish_color_end
+# the color for process separators like ';' and '&'
+set fish_color_end magenta
 
-# # the color used to highlight potential errors
-# set fish_color_error
+# the color used to highlight potential errors
+set fish_color_error 9D3F3D
 
-# # the color for regular command parameters
-# set fish_color_param
+# the color for regular command parameters
+set fish_color_param normal
 
-# # the color used for code comments
+# the color used for code comments
 # set fish_color_comment
 
-# # the color used to highlight matching parenthesis
-# set fish_color_match
+# the color used to highlight matching parenthesis
+set fish_color_match yellow
 
-# # the color used when selecting text (in vi visual mode)
+# the color used when selecting text (in vi visual mode)
 # set fish_color_selection
 
-# # used to highlight history search matches and the selected pager item (must be a background)
+# used to highlight history search matches and the selected pager item (must be a background)
 # set fish_color_search_match
 
-# # the color for parameter expansion operators like '*' and '~'
-# set fish_color_operator
+# the color for parameter expansion operators like '*' and '~'
+set fish_color_operator normal
 
-# # the color used to highlight character escapes like '\n' and '\x70'
-# set fish_color_escape
+# the color used to highlight character escapes like '\n' and '\x70'
+set fish_color_escape magenta
 
-# # the color used for the current working directory in the default prompt
+# the color used for the current working directory in the default prompt
 # set fish_color_cwd
 
-# # the color used for autosuggestions
+# the color used for autosuggestions
 # set fish_color_autosuggestion
 
-# # the color used to print the current username in some of fish default prompts
+# the color used to print the current username in some of fish default prompts
 # set fish_color_user
 
-# # the color used to print the current host system in some of fish default prompts
+# the color used to print the current host system in some of fish default prompts
 # set fish_color_host
 
-# # the color for the '^C' indicator on a canceled command
-# set fish_color_cancel
+# the color for the '^C' indicator on a canceled command
+set fish_color_cancel cyan
 
 # Additionally, the following variables are available to change the highlighting in the completion pager:
 
