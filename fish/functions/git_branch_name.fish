7041e21509c6476a79fdc847e1afc42f0ff53b07
Dan Cheail <dan@dnch.io>
2019-10-16 18:42:20 +1100

    Use starship for git/right-prompt goodness

--- fish/functions/git_branch_name.fish
+++
@@ -1,3 +0,0 @@
-function git_branch_name
-	echo (command git symbolic-ref HEAD 2> /dev/null | sed -e 's|^refs/heads/||')
-end
--- fish/functions/git_dirty_status.fish
+++
@@ -1,7 +0,0 @@
-function git_dirty_status
-	if [ -z (command git status --porcelain 2> /dev/null | tail -n1) ]
-		echo (set_color 9c3)" ±"(set_color normal)
-	else
-		echo (set_color c03)" ±"(set_color normal)
-	end
-end
