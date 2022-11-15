c103457c0e23bdb68fde9e6cd6ac3f1c9c68e883
Dan Cheail <dan@dnch.xyz>
2022-11-16 09:46:34 +1100

    remove ironic_greeting_messages

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,11 +1,4 @@
 function fish_greeting
-  set -a ironic_greeting_messages "**** COMMODORE 64 BASIC V2 ****"
-  set -a ironic_greeting_messages "Microsoft(R) MS-DOS(R) Version 6.22"
-  set -a ironic_greeting_messages "Starting Windows ME..."
-
   dotfiles_display_status
-  echo
-  echo
-  echo (random choice $ironic_greeting_messages)
   echo
 end
