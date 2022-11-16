2d04d9920104cb28adb46f78b4d7270d663613f8
Dan Cheail <dan.cheail@cultureamp.com>
2022-11-16 11:57:04 +1100

    fixing status message

--- fish/functions/dotfiles_display_status.fish
+++ fish/functions/dotfiles_display_status.fish
@@ -1,17 +1,20 @@
 function dotfiles_display_status
   if set -qu DOTFILES_STALE
-    set segment_background 00B100
+    set segment_background $theme_color_alert_green
     set message "Upstream changes available"
   else if set -qu DOTFILES_DIRTY
-    set segment_background FF8A04
+    set segment_background $theme_color_alert_orange
     set message "Uncomitted changes in $XDG_CONFIG_HOME"
   else if set -qu DOTFILES_SHINY
-    set segment_background 055DF2
+    set segment_background $theme_color_alert_orange
     set message "Unpushed changes in $XDG_CONFIG_HOME"
+  else
+    set segment_background $theme_color_alert_red
+    set message "I dunno, your guess is as good as mine"
   end
 
   set prompt_segments (powerline_segment $segment_background (set_color fff; echo 'dotfiles'))
-  set prompt_segments $prompt_segments (powerline_segment 080804 (set_color normal; echo $message))
+  set prompt_segments $prompt_segments (powerline_segment $theme_color_neutral_500 (set_color $theme_color_neutral_200; echo $message))
 
   powerline $prompt_segments
 end
