cbb3983a140cc9bdf8bfb4ae181d67b79252ddc6
Dan Cheail <dan@dnch.xyz>
2023-05-05 19:29:48 +1000

    feat: re-add rubygems niceness

--- fish/config.fish
+++ fish/config.fish
@@ -10,6 +10,9 @@ source $XDG_CONFIG_HOME/fish/lscolors.fish
 # load git helpers into path
 set -x PATH $XDG_CONFIG_HOME/git/helpers $PATH
 
+ # encourage rubygems to play nicely
+ set -x GEMRC $XDG_CONFIG_HOME/gem/config.yaml
+
 # vscode is the _best_ code
 set -x EDITOR "code --wait"
 
